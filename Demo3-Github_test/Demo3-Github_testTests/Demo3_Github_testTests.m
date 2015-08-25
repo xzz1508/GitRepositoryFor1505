673340ea3257cd4a8913d53d1ee9b90eeb688e75
tarena <tarena@tarenadeiMac-12.local>
2015-08-25 16:31:18 +0800

    创建一个新的工程

---
+++ Demo3-Github_test/Demo3-Github_testTests/Demo3_Github_testTests.m
@@ -0,0 +1,40 @@
+//
+//  Demo3_Github_testTests.m
+//  Demo3-Github_testTests
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Demo3_Github_testTests : XCTestCase
+
+@end
+
+@implementation Demo3_Github_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
